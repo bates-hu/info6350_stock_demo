c6c1117a94e6b2acb3a3c2917bb44fad6c0df810
bates <hbc0204@foxmail.com>
2020-10-29 16:15:06 -0700

    Initial Commit

---
+++ stockDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockDemo
+//
+//  Created by 胡秉诚 on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
